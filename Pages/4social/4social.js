8925e33ea8a8965d7824743c9abf4f4c4588e97a
tombaladron <120615270+tombaladron@users.noreply.github.com>
2022-12-30 21:40:32 -0300

    v0

--- Pages/4social/4social.js
+++
@@ -1,9 +0,0 @@
-$(".click1").click(function () {
-    var elClickeado = $(this).attr('id');
-    $("." + elClickeado).toggleClass("hidden");
-});
-
-$(".click2").click(function () {
-    var elClickeadob = $(this).attr('id');
-    $("." + elClickeadob).toggleClass("hidden");
-});
